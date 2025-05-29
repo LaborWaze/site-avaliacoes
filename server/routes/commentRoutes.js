cdf5d402f8edcffa57eb25156c9884a59227645e
LaborWaze <laborwaze@gmail.com>
2025-05-29 02:21:14 -0300

    Update commentRoutes.js
    test delet

--- server/routes/commentRoutes.js
+++ server/routes/commentRoutes.js
@@ -2,7 +2,8 @@ import { Router } from 'express';
 import {
   postComment,
   getComments,
-  likeComment
+  likeComment,
+  deleteComment
 } from '../controllers/commentController.js';
 
 const router = Router();
@@ -10,5 +11,6 @@ const router = Router();
 router.post('/', postComment);
 router.get('/', getComments);
 router.post('/:id/like', likeComment);
+router.delete('/:id', deleteComment);
 
 export default router;
